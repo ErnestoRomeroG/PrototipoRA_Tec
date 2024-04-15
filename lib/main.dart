e780310ee7d6a8cb3ae68147ff2a9b6cd774ff5c
Arturox21 <artur.vglz@gmail.com>
2024-04-14 19:45:14 -0500

    Prueba de branch 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demos',
+      title: 'Conoce el ITM con AR',
       theme: ThemeData(
         // This is the theme of your application.
         //
