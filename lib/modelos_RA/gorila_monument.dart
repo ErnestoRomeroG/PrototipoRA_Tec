fd23007ea83b42e457d5bc455658e865c06e4bfa
ErnestoRomeroG <ernestosmitch@gmail.com>
2024-04-20 19:41:04 -0500

    Plantilla

---
+++ lib/modelos_RA/gorila_monument.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+class monumentogorila extends StatelessWidget {
+  const monumentogorila({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Scaffold(
+            body: ModelViewer(
+      src: "lib/modelos/gorila.glb",
+      ar: true,
+    )));
+  }
+}
